356d286cc758a42e52a050f9b4fef6f1c80b036e
Elaine <elaine.martis@wowlabz.com>
2018-01-20 15:45:47 +0530

    web3js file

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "oil minimum illness rescue tourist tree series gesture pave dose jump apology gas caution nature";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/AzPNR6IGk31xJWmPGDte")
+      },
+      network_id: 3,
+      gas: 500000
     }
   }
 };
