07bc041c061adafd026318e083d591776bc05766
Elaine <elaine.martis@wowlabz.com>
2018-01-31 15:21:03 +0530

    create department

---
+++ src/js/dept.js
@@ -0,0 +1,351 @@
+$(function() {
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } 
+    else {
+      // set the provider you want from Web3.providers
+      //  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+       web3 = new Web3(new Web3.providers.HttpProvider("http://35.154.203.141:8545"));
+      // web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/AzPNR6IGk31xJWmPGDte"));
+    }
+
+    var tokenContract = new web3.eth.Contract([
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "names",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "contracts",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "i",
+              "type": "uint256"
+            }
+          ],
+          "name": "getSymbol",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "i",
+              "type": "uint256"
+            }
+          ],
+          "name": "getName",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [],
+          "name": "getNameSize",
+          "outputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "i",
+              "type": "uint256"
+            }
+          ],
+          "name": "getAddress",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "symbols",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "constant": false,
+          "inputs": [
+            {
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "name": "symbol",
+              "type": "string"
+            }
+          ],
+          "name": "createAssetContract",
+          "outputs": [
+            {
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+        }
+      ], "0xf38abf53d19bebf224064c23dfe0cd503e8d2a5d");
+
+    var deptContract = new web3.eth.Contract([
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "name": "_email",
+          "type": "string"
+        },
+        {
+          "name": "_incomingAsset",
+          "type": "address"
+        },
+        {
+          "name": "_outgoingAsset",
+          "type": "address"
+        }
+      ],
+      "name": "createTempDepartment",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getDepartmentsSize",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "name": "assetContractAddr",
+          "type": "address"
+        }
+      ],
+      "name": "transferAsset",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "index",
+          "type": "uint256"
+        }
+      ],
+      "name": "getDepartmentAccAddr",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_email",
+          "type": "string"
+        },
+        {
+          "name": "_accAddr",
+          "type": "address"
+        }
+      ],
+      "name": "createDepartment",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "email",
+          "type": "string"
+        }
+      ],
+      "name": "getAccWithEmail",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_accAddr",
+          "type": "address"
+        }
+      ],
+      "name": "getDepartment",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "address"
+        },
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ], "0xfbfc94e5cec3ca42aa5641db6e0b32c694388648");
+
+    $('.createDeptBtn').click(function(){
+    	var name = $('#deptName').val();
+    	var email = $('#deptEmail').val();
+    	
+    });
+
+    $(window).load(function() {
+        tokenContract.methods.getNameSize().call(function(error, size){
+            var symbolCounter = 0;
+            for(var i=0; i<size; i++) {
+                let index = i;
+                tokenContract.methods.getName(index).call().then(function(result){
+                    let assetName = result;
+                    tokenContract.methods.getSymbol(index).call().then(function(result){
+                        let assetSymbol = result;
+                        $('.incomingAsset').append('<option value="0x58a6C8e51BF13afd515c8B0C3Aa918ebB08CA5D1">'+assetName+' - '+assetSymbol+'</option>');
+                        $('.outgoingAsset').append('<option value="0x58a6C8e51BF13afd515c8B0C3Aa918ebB08CA5D1">'+assetName+' - '+assetSymbol+'</option>');
+                    });
+                });
+            }
+        });
+    });
+
+
+});
